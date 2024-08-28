bbbe746193b6ae878a5b26cf5edf48104d874a7c
Лапшин Илья Романович <irl1@tpu.ru>
2024-08-29 03:21:24 +0700

    Поправлен axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,11 +1,9 @@
-import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 
 const axiosInstance = axios.create({
     baseURL: 'https://shift-intensive-potato-wallet.onrender.com/potato/api',
 });
-const navigate = useNavigate();
 
 
 axiosInstance.interceptors.request.use(config => {
@@ -24,7 +22,7 @@ axiosInstance.interceptors.response.use(
             localStorage.removeItem('sessionToken');
 
             // Если вы используете React Router, перенаправьте пользователя
-            navigate('/login') // или используйте useNavigate
+            window.location.href = '/login'; // или используйте useNavigate
         }
         return Promise.reject(error);
     }
